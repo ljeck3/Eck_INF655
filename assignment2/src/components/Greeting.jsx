3fdb3b37866a1aef6f312fa93f1c6240e9d137b2
lj-1003 <levijustin24@gmail.com>
2026-03-02 12:24:47 -0600

    Task 1 add file

---
+++ assignment2/src/components/Greeting.jsx
@@ -0,0 +1,13 @@
+function Greeting(props) {
+  const date = new Date();
+
+  return (
+    <div>
+      <h1>Hello, {props.username}</h1>
+      <p style={{ color: "blue" }}>React is really cool!</p>
+      <p>Today's date: {date.toDateString()}</p>
+    </div>
+  );
+}
+
+export default Greeting;
