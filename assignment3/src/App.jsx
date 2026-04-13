bcabf809057d7a80c578c98fd75e801b085f5853
lj-1003 <levijustin24@gmail.com>
2026-04-12 21:59:31 -0500

    Username shows on greeting.

--- assignment3/src/App.jsx
+++ assignment3/src/App.jsx
@@ -1,17 +1,27 @@
 import { useState, useEffect } from 'react';
+
+import { auth } from '../firebase.js'
+
 import Greeting from './components/Greeting'
 import UserInfo from './components/UserInfo'
 import Counter from './components/Counter'
 import TaskForm from './components/TaskForm'
 import Register from './components/Register';
 import Login from './components/Login';
+import Logout from './components/Logout';
+import { onAuthStateChanged } from 'firebase/auth';
 
 function App() {
 const name1 = "Marquis Nordyke"
 const name2 = "Chris P. Bacon"
 
 const [user, setUser] = useState(null)
-
+useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser)
+    })
+    return unsubscribe
+}, [])
 
 function handleAlert() {
     alert("This is an alert");
@@ -19,9 +29,10 @@ function handleAlert() {
 
   return (
     <div className="App">
+      <Logout />
       <Register />
       <Login />
-      <Greeting username={name1} />
+      <Greeting username={user ? user.email : "Guest"} />
       <hr></hr>
       <UserInfo handleClick={handleAlert} />
       <hr></hr>
---
+++ assignment3/src/components/Logout.jsx
@@ -0,0 +1,21 @@
+import { useAuth } from '../context/AuthContext.jsx';
+
+  function Logout() {
+    const { logout } = useAuth()
+
+    const handleLogout = async () => {
+        try {
+            await logout();
+            alert("Logged out successfully");
+        } catch (error) {
+            console.error("Logout error:", error.message);
+        }
+    }
+    return (
+    <div>
+            <button onClick={handleLogout}>Logout</button>
+    </div>
+    );
+}
+
+export default Logout;
